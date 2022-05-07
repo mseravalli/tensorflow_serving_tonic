4cbb2b369d031c18cabc90842ad8396ecce83e8c
Marco Seravalli <marco.seravalli@gmail.com>
2022-05-07 11:16:50 +0000

    Export library in cargo

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod tensorflow {
-    pub mod serving {
-        include!("tensorflow.serving.rs");
-    }
     pub mod error {
         include!("tensorflow.error.rs");
+    }
+    pub mod serving {
+        include!("tensorflow.serving.rs");
     }
     include!("tensorflow.rs");
 }
